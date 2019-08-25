4eef4a6f575471d0e13b6f2265c36c59f76ac926
TroddenSpade <parsasam1378@gmail.com>
2019-08-26 02:15:24 +0430

    Create Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Logo />
+    <Header />
+    <Main />
+  </div>
+</template>
+
+<script>
+import Logo from "./Logo";
+import Header from "./Header";
+import Main from "./Main";
+
+export default {
+  name: "home",
+  components: {
+    Logo,
+    Header,
+    Main
+  }
+};
+</script>
+
+<style >
+@import url("https://fonts.googleapis.com/css?family=Teko:700&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Anton&display=swap");
+body {
+  margin: 0;
+}
+</style>
