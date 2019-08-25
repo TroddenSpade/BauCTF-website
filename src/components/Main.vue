63908077158186d506375e7367a6299858308409
TroddenSpade <parsasam1378@gmail.com>
2019-08-25 19:45:11 +0430

    Create Main.vue

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <What />
+    <Who />
+    <How />
+    <Team />
+  </div>
+</template>
+
+<script>
+import What from "./main/What";
+import Who from "./main/Who";
+import How from "./main/How";
+import Team from "./main/Team";
+export default {
+  name: "main",
+  components: {
+    What,
+    Who,
+    How,
+    Team
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  height: 200vh;
+}
+</style>
