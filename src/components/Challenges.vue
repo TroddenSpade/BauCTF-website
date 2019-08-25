bca6e420fd9fb922981862bfb918fdf07f086fef
TroddenSpade <parsasam1378@gmail.com>
2019-08-26 02:13:30 +0430

    initial challenges and scoreboard vue files added

---
+++ src/components/Challenges.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main">
+    <p>challenges</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "challenges"
+};
+</script>
+
+
+<style scoped>
+.navbar {
+  height: 10vh;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+}
+
+.fake-navbar {
+  height: 10vh;
+}
+
+.img {
+  height: 10vh;
+}
+
+.sticky-navbar {
+  position: fixed;
+  top: 0%;
+  width: 100%;
+  border-bottom: solid yellow;
+  border-bottom-width: 3px;
+}
+
+a {
+  font-family: "Teko", sans-serif;
+  font-size: 3vh;
+  text-decoration: none;
+  color: grey;
+}
+
+.active {
+  color: black;
+}
+</style>
