97265bce262d8f6bd238ea785ca7b49296d1e244
TroddenSpade <parsasam1378@gmail.com>
2019-08-25 15:06:41 +0430

    Create Logo.vue

---
+++ src/components/Logo.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="logo">
+    <img class="img" src="../assets/logo.png" />
+    <p>Scroll Down</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "logo"
+};
+</script>
+
+<style scoped>
+.logo {
+  display: flex;
+  flex: 1;
+  background-color: yellow;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.img {
+  height: 70%;
+}
+
+p {
+  font-family: "Teko", sans-serif;
+  font-size: 3vh;
+  animation: scrollDown 2s infinite;
+  -webkit-animation: scrollDown 2s infinite;
+  -moz-animation: scrollDown 2s infinite;
+}
+
+@keyframes scrollDown {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@-webkit-keyframes scrollDown {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@-moz-keyframes scrollDown {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
