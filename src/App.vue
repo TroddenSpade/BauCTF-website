21b2186b4eb1c49f77d56159fb3d32c3a0765025
TroddenSpade <parsasam1378@gmail.com>
2019-08-25 15:07:32 +0430

    logo header and main components added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <Logo />
+    <Header />
+    <Main />
+  </div>
 </template>
 
 <script>
+import Logo from "./components/Logo";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 export default {
   name: "app",
   data() {
     return {};
+  },
+  components: {
+    Logo,
+    Header,
+    Main
   }
 };
 </script>
 
 <style >
+@import url("https://fonts.googleapis.com/css?family=Teko:700&display=swap");
+
+body {
+  margin: 0;
+}
 </style>
