08cfc53a9eba832f0bc6490ea8c710c6900e1ab4
TroddenSpade <parsasam1378@gmail.com>
2019-08-27 02:01:51 +0430

    flag image added

--- src/components/main/What.vue
+++ src/components/main/What.vue
@@ -4,7 +4,9 @@
       <h1>What is CTF ?</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing vitae proin sagittis nisl rhoncus mattis. Vel facilisis volutpat est velit egestas dui id ornare. Bibendum arcu vitae elementum curabitur vitae nunc sed. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Cursus vitae congue mauris rhoncus. Enim nulla aliquet porttitor lacus. Viverra nam libero justo laoreet sit amet cursus sit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas. Parturient montes nascetur ridiculus mus mauris vitae. Magna fermentum iaculis eu non diam phasellus vestibulum lorem. Risus feugiat in ante metus dictum at. Ut enim blandit volutpat maecenas. Arcu dui vivamus arcu felis. Amet facilisis magna etiam tempor orci eu lobortis elementum. Habitant morbi tristique senectus et netus et malesuada fames. Varius quam quisque id diam vel quam elementum pulvinar etiam. Velit egestas dui id ornare arcu odio. Euismod in pellentesque massa placerat.</p>
     </div>
-    <div class="half2"></div>
+    <div class="half2">
+      <img class="flag" src="../../assets/flag.png" alt />
+    </div>
   </div>
 </template>
 
@@ -34,10 +36,16 @@ export default {
 
 .half2 {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
+  align-items: center;
   width: 50%;
   height: 100%;
+}
+
+.flag {
+  width: auto;
+  height: 40vh;
 }
 
 h1 {
