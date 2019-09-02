220df874b06bc996ed8efb18e8faeacf146c4740
Parsa Sam <parsasam1378@gmail.com>
2019-09-02 15:13:29 +0430

    updated

--- src/components/Challenges.vue
+++ src/components/Challenges.vue
@@ -1,51 +1,110 @@
 <template>
   <div class="main">
-    <p>challenges</p>
+    <div class="challenge" v-for="(challenge, index) in challenges" :key="index">
+      <button
+        class="collapsible"
+        v-on:click="collapse(index)"
+      >{{challenge.name}} ( {{challenge.point}} )</button>
+      <div class="content" ref="challenge">
+        <p>{{challenge.body}}</p>
+        <a :href="challenge.attach" target="_blank">[ attached file ]</a>
+        <br />
+        <br />
+
+        <input class="name" type="text" placeholder="Your Team" />
+        <br />
+        <input type="text" placeholder="CTF{ ... }" />
+        <br />
+        <button class="submit">Submit</button>
+        <br />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import challenges from "../../data/challenges.json";
 export default {
-  name: "challenges"
+  name: "challenges",
+  data: function() {
+    return {
+      challenges,
+      isActive: Array(challenges.length).fill(0)
+    };
+  },
+  methods: {
+    collapse(i) {
+      if (this.isActive[i]) {
+        this.$refs.challenge[i].classList.remove("active");
+      } else {
+        this.$refs.challenge[i].classList.add("active");
+      }
+      this.isActive[i] = 1 - this.isActive[i];
+    }
+  }
 };
 </script>
 
 
 <style scoped>
-.navbar {
-  height: 10vh;
-  flex: 1;
+.main {
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  flex: 1;
+  justify-content: center;
   align-items: center;
-  background-color: white;
+  flex-direction: column;
 }
 
-.fake-navbar {
-  height: 10vh;
+.challenge {
+  width: 80vw;
+  border-bottom-width: 1px;
+  border-bottom: solid;
+  border-bottom-color: lightgrey;
 }
 
-.img {
-  height: 10vh;
-}
-
-.sticky-navbar {
-  position: fixed;
-  top: 0%;
+.collapsible {
+  font-family: "Anton", sans-serif;
+  background-color: rgb(255, 255, 148);
+  color: black;
+  cursor: pointer;
+  padding: 18px;
   width: 100%;
-  border-bottom: solid yellow;
-  border-bottom-width: 3px;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
 }
 
-a {
-  font-family: "Teko", sans-serif;
-  font-size: 3vh;
-  text-decoration: none;
-  color: grey;
+.collapsible:hover {
+  background-color: rgb(255, 238, 82);
+}
+
+.collapsible:after {
+  color: white;
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  padding: 0 18px;
+  overflow: hidden;
+  max-height: 0;
+  transition: max-height 0.2s ease-out;
+  background-color: #f1f1f1;
 }
 
 .active {
-  color: black;
+  max-height: 50vh;
+}
+
+.name {
+  width: 30%;
+}
+
+.submit {
+  width: 10vw;
 }
 </style>
--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,50 +1,112 @@
 <template>
   <div class="main">
-    <p>Score Board</p>
+    <div class="table">
+      <div class="head-table">
+        <div class="name">
+          <h1>Teams</h1>
+        </div>
+        <div class="problem" v-for="index in 12" :key="index">
+          <h1>{{index}}</h1>
+        </div>
+        <div class="score">
+          <h1>Score</h1>
+        </div>
+      </div>
+
+      <div class="team" v-for="(team,index) in teams" :key="index">
+        <h2>{{index+1}}</h2>
+        <h1 class="name">{{team.name}}</h1>
+        <div class="problem" v-for="index in 12" :key="index">
+          <h1>-</h1>
+        </div>
+        <div class="score">
+          <h1>{{team.score}}</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import teams from "../../data/teams.json";
 export default {
-  name: "score-board"
+  name: "score-board",
+  data: function() {
+    return {
+      teams
+    };
+  },
+  methods: {}
 };
 </script>
 
 <style scoped>
-.navbar {
-  height: 10vh;
-  flex: 1;
+.main {
+  min-height: 90vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 5vh;
+  padding-bottom: 5vh;
+}
+
+.table {
+  width: 90vw;
+}
+
+.head-table {
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  background-color: white;
-}
-
-.fake-navbar {
-  height: 10vh;
-}
-
-.img {
-  height: 10vh;
-}
-
-.sticky-navbar {
-  position: fixed;
-  top: 0%;
   width: 100%;
-  border-bottom: solid yellow;
-  border-bottom-width: 3px;
-}
-
-a {
+  height: 10vh;
+  border: solid;
+  border-color: lightgrey;
+  border-width: 0.5px;
+  border-bottom-color: lightgray;
+  border-bottom-width: 1px;
+  background-color: yellow;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
   font-family: "Teko", sans-serif;
-  font-size: 3vh;
-  text-decoration: none;
-  color: grey;
 }
 
-.active {
-  color: black;
+.name {
+  width: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.problem {
+  width: 6%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.score {
+  width: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.team {
+  width: 100%;
+  height: 10vh;
+  background-color: white;
+  border-bottom: solid;
+  border-right: solid;
+  border-width: 1px;
+  border-color: lightgrey;
+  border-bottom-right-radius: 10px;
+  display: flex;
+  flex-direction: row;
+}
+
+h2 {
+  font-family: "Teko", sans-serif;
 }
 </style>
