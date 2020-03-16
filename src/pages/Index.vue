cfee80d04b039ab6554afe0b7f9b007d7c097fa6
TroddenSpade <parsasam1378@gmail.com>
2020-03-16 22:36:59 +0330

    Create Index.vue

---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Logo />
+
+    <Layout :stickable="false">
+      <Info />
+      <Team />
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import Info from "~/components/Info.vue";
+import Team from "~/components/Team.vue";
+
+export default {
+  metaInfo: {
+    title: "KNTU CTF"
+  },
+  components: {
+    Logo,
+    Info,
+    Team
+  }
+};
+</script>
+
+<style scoped>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
