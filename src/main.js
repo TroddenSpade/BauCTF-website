5beb3e63e686b9f74a3805ff3f8123b005a76567
TroddenSpade <parsasam1378@gmail.com>
2020-05-25 02:13:51 +0430

    echo and socket.io added

--- src/main.js
+++ src/main.js
@@ -4,15 +4,27 @@
 import "~/assets/index.css";
 
 import Vuex from "vuex";
-import axios from "axios";
+import Echo from "laravel-echo";
+import io from "socket.io-client";
 
 import HeadertLayout from "~/layouts/Header.vue";
 import { store } from "~/store";
 
 export default function(Vue, { router, head, isClient, appOptions }) {
   // Set default layout as a global component
+  head.meta.push({
+    name: "viewport",
+    content: "width=device-width, initial-scale=1"
+  });
+
   Vue.component("Layout", HeadertLayout);
   Vue.use(Vuex);
   appOptions.store = store;
   appOptions.store.dispatch("autoLogin");
+
+  Vue.prototype.$echo = new Echo({
+    broadcaster: "socket.io",
+    host: "localhost:6001",
+    client: io
+  });
 }
