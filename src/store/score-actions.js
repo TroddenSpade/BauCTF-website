d424c07787a1c4dabe34a88f8fbb657d6f09ed75
TroddenSpade <parsasam1378@gmail.com>
2020-05-27 00:36:14 +0430

    Update score-actions.js
    submissions and leaderboard actions added

--- src/store/score-actions.js
+++ src/store/score-actions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { SCOREBOARD } from "./links";
+import { SCOREBOARD, LEADERBOARD, SUBMISSIONS } from "./links";
 
 export default {
   scoreboard({ commit }, { cb }) {
@@ -9,6 +9,42 @@ export default {
         if (res.data) {
           await commit("scoreboard", res.data);
           cb();
+        }
+      })
+      .catch((err) => {
+        if (err.response) return alert(err.response.data.error);
+        return alert(err);
+      });
+  },
+  leaderboard({ commit }) {
+    axios
+      .get(LEADERBOARD, {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        if (res.data) {
+          commit("leaderboard", res.data);
+        }
+      })
+      .catch((err) => {
+        if (err.response) return alert(err.response.data.error);
+        return alert(err);
+      });
+  },
+  submissions({ commit }) {
+    axios
+      .get(SUBMISSIONS, {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        if (res.data) {
+          commit("submissions", res.data);
         }
       })
       .catch((err) => {
