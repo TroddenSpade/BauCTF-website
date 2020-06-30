4b0089ebae851031ff903e1474803eda627bd393
troddenspade <parsasam1378@gmail.com>
2020-06-30 15:12:54 -0700

    updated

--- static/js/options.js
+++ static/js/options.js
@@ -7,26 +7,26 @@ particlesJS(
         value: 160,
         density: {
           enable: true,
-          value_area: 800,
-        },
+          value_area: 800
+        }
       },
       color: {
-        value: "#149414",
+        value: "#149414"
       },
       shape: {
         type: "circle",
         stroke: {
           width: 0,
-          color: "#000000",
+          color: "#000000"
         },
         polygon: {
-          nb_sides: 5,
+          nb_sides: 5
         },
         image: {
           src: "img/github.svg",
           width: 100,
-          height: 100,
-        },
+          height: 100
+        }
       },
       opacity: {
         value: 1,
@@ -35,8 +35,8 @@ particlesJS(
           enable: true,
           speed: 1,
           opacity_min: 0,
-          sync: false,
-        },
+          sync: false
+        }
       },
       size: {
         value: 3,
@@ -45,15 +45,15 @@ particlesJS(
           enable: false,
           speed: 4,
           size_min: 0.3,
-          sync: false,
-        },
+          sync: false
+        }
       },
       line_linked: {
         enable: false,
         distance: 150,
         color: "#ffffff",
         opacity: 0.4,
-        width: 1,
+        width: 1
       },
       move: {
         enable: true,
@@ -66,49 +66,49 @@ particlesJS(
         attract: {
           enable: false,
           rotateX: 600,
-          rotateY: 600,
-        },
-      },
+          rotateY: 600
+        }
+      }
     },
     interactivity: {
       detect_on: "canvas",
       events: {
         onhover: {
-          enable: true,
-          mode: "bubble",
+          enable: false,
+          mode: "bubble"
         },
         onclick: {
-          enable: true,
-          mode: "repulse",
+          enable: false,
+          mode: "repulse"
         },
-        resize: true,
+        resize: true
       },
       modes: {
         grab: {
           distance: 400,
           line_linked: {
-            opacity: 1,
-          },
+            opacity: 1
+          }
         },
         bubble: {
           distance: 250,
           size: 0,
           duration: 2,
           opacity: 0,
-          speed: 3,
+          speed: 3
         },
         repulse: {
           distance: 400,
-          duration: 0.4,
+          duration: 0.4
         },
         push: {
-          particles_nb: 4,
+          particles_nb: 4
         },
         remove: {
-          particles_nb: 2,
-        },
-      },
+          particles_nb: 2
+        }
+      }
     },
-    retina_detect: true,
+    retina_detect: true
   }
 );
